18d7404478e59a8d648e7ba43793cbafce5cef28
Jalal <jalaluddinmps@gmail.com>
2026-03-14 22:19:02 +0500

    making an update function

--- 1_pytdantic_why.py
+++ 1_pytdantic_why.py
@@ -6,6 +6,16 @@ def insert_patient_data(name: str, age: int):
     else:
         raise TypeError("name must be a string and age must be an integer")
 
-    insert_patient_data("jalal", 27)
+insert_patient_data("jalal", 27)
+
+def update_patient_data(name: str, age: int):
+    if type(name)==str  and type(age)==int:
+        print(name)
+        print(age)
+        print("data updated in database")
+    else:
+        raise TypeError("name must be a string and age must be an integer")
+
+update_patient_data("jalal", 27)
 
  
