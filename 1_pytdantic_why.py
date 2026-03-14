6ef816ce532440d1d3ddc82f0e8000f4f3b40529
Jalal <jalaluddinmps@gmail.com>
2026-03-14 22:25:22 +0500

    adding both data and type validation in update function

--- 1_pytdantic_why.py
+++ 1_pytdantic_why.py
@@ -8,11 +8,15 @@ def insert_patient_data(name: str, age: int):
 
 insert_patient_data("jalal", 27)
 
+#update function 
 def update_patient_data(name: str, age: int):
     if type(name)==str  and type(age)==int:
-        print(name)
-        print(age)
-        print("data updated in database")
+        if age < 0:
+            raise ValueError("age must be a positive integer")
+        else:
+            print(name)
+            print(age)
+            print("data updated in database")
     else:
         raise TypeError("name must be a string and age must be an integer")
 
