ef307054dd9830a0a44a048fb86dbc339704892a
Jalal <jalaluddinmps@gmail.com>
2026-03-15 23:43:11 +0500

    some changes to the class

---
+++ 02_pydantic_class.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel 
+
+class Patient(BaseModel):
+    name: str
+    age: int
+    
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("inserted into database")
+
+patient_info = {'name': 'jalal', 'age': 27}
+patient1= Patient(**patient_info)
+
+insert_patient_data(patient1)
+
