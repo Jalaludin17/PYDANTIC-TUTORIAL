26d20d43e2385a5b3a32986f149e0a83d1ed37fa
Jalal <jalaluddinmps@gmail.com>
2026-03-17 23:30:51 +0500

    defined a new function and testing pydanctic functionality

--- 02_pydantic_class.py
+++ 02_pydantic_class.py
@@ -9,9 +9,16 @@ def insert_patient_data(patient: Patient):
     print(patient.age)
     print("inserted into database")
 
-patient_info = {'name': 'jalal', 'age': 27}
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print("updated into database")
+
+#if age is added as a string, it will be converted to int by pydantic(only if it is a number with quotes.)
+patient_info = {'name': 'jalal', 'age': '27'}
 patient1= Patient(**patient_info)
 
-insert_patient_data(patient1)
+update_patient_data(patient1)
+
 
 
