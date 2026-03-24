45e8895ef317779f1b47c8466caeb0d4c6cd8c97
Jalal <jalaluddinmps@gmail.com>
2026-03-25 00:15:17 +0500

    new commit

--- 05_pydantic_model_validation.py
+++ 05_pydantic_model_validation.py
@@ -11,4 +11,33 @@ class Patient(BaseModel):
     allergies: List[str] 
     contact_details: Dict[str, str]
 
-   
+    @model_validator(mode='after')
+    def validate_emergency_contact(cls, model):
+        if model.age > 60 and 'emergency_contact' not in model.contact_details:
+            raise ValueError("Emergency contact is required in contact details")
+        return model 
+    
+def update_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.email)
+    print(patient.linked_url)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print("updated into database")
+
+pateint_info = {
+    'name': 'jalal',
+    'email':'abc@ICB.com',
+    'linked_url': 'https://www.example.com',   
+    'age': 39,
+    'weight': 70.5,
+    'married': True,
+    'allergies': ['pollen', 'dust'],
+    'contact_details': {'phone': '023-45678900'}
+}
+patient1 = Patient(**pateint_info)
+
+update_patient_data(patient1)
