70d5ae1f055c4387ab8d1faa89b6ee115fa8d161
Jalal <jalaluddinmps@gmail.com>
2026-03-25 00:11:12 +0500

    update

--- 03_pydantic_class_02.py
+++ 03_pydantic_class_02.py
@@ -1,19 +1,23 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):  
-    name: str = Field(max_length=50)
+    name: Annotated[str, Field(max_length=50, title="Patient Name", description="The name of the patient. It should be a string with a maximum length of 50 characters.", examples=["John Doe", "Ali"])] #max_length is used to set the maximum length of the string, title and description are used for documentation purposes.
     email: EmailStr  #custom type for email validation
     linked_url: Optional[AnyUrl] = None #custom type for url validation, 
     age: int = Field(gt=15, lt=40)
-    weight: float = Field(gt=0) #gt means greater than 0
-    married: bool
-    allergies: Optional[List[str]] = None  # imported List to use here
+    weight: Annotated[float, Field(gt=0, strict=True)] #gt means greater than 0
+    married: Annotated[bool, Field(default=None, description="Marital status of the patient. ")] #description is used for documentation purposes.
+    allergies: Annotated[Optional[List[str]], Field(default=None, description="List of allergies the patient has. ")] #description is used for documentation purposes.
     contact_details: Dict[str, str]
 
 #all above fields are required, if any of them is missing, it will raise a validation error. 
 # To make them optional, we can use Optional from typing module and set default value to None.
 #as used for ellergies
+
+#The Field function is not only used to set defualt values and constraints, 
+# but also for adding metadata to the fields, which can be used for documentation purposes 
+# or for generating API schemas. it is used with Annotated from typing module.
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
